2d8ee4f09f2adad669e28010f58f85dca6e438ff
Vishnat Pal <vishntpal021@gmail.com>
2026-02-08 12:47:21 +0530

    Code of Day_7 JavaScrit started

---
+++ Day_7/index.js
@@ -0,0 +1,15 @@
+
+var age  = 18
+if (age < 5){
+ console.log("You are allowed to vote")
+}
+else{
+    console.log("You are not allowed to vote")
+}
+
+for (var i = 1; i<=100; i+=3){
+    console.log(i)
+}
+
+
+// https://classroom.github.com/a/fwCZoRgs
